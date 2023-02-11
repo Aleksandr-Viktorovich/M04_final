364283ee36cfdabd7e5870991db3c4ccba84fa7f
Aleksandr-Viktorovich <teanvkz@gmail.com>
2023-02-12 03:47:46 +0700

    01

--- game.js
+++ game.js
@@ -10,37 +10,41 @@ const game = () => {
 
   return function start () {
     const userPlay = () => {
-      let userResult = '';
+      let userResult;
       let userItem = +prompt(`Выбери количество шариков, на которое хочешь сыграть! \nУ тебя ${result.user} шариков!`, '');
       if (Number.isNaN(userItem)) {
         alert('Ты ввел не корректное значение');
         return userPlay();
       }
-      if (userItem) {
         if (userItem <= result.user && userItem <= result.SkyNet) {
           userResult = userItem;
         } else {
           alert('У тебя нет столько шариков');
           return userPlay();
         }
-      }
-      if (userItem === null || userItem === 0) {
+      if (userItem === 0) {
         let who = confirm('Сдулся кожаный?=))');
+        console.log(who)
         if (who) {
-          return alert(`Счет: \nКожаный - ${result.user} \nSkyNet - ${result.SkyNet}`);
+          alert(`Счет: \nКожаный - ${result.user} \nSkyNet - ${result.SkyNet}`);
+          return;
         } else {
           return userPlay();
         }
       }
       return userResult;
     };
+
     const skyNetPlay = () => {
       let skyNetResult = Math.round(Math.random());
       return skyNetResult;
     }
+
      skyNetPlay()
+
     let choiceUser = userPlay();
     let choiceSkyNet = skyNetPlay();
+
     const winner = () => {
       if (choiceUser % 2 === 0 && choiceSkyNet % 2 === 0) {
         result.user -= choiceUser;
@@ -62,16 +66,14 @@ const game = () => {
         result.SkyNet -= choiceUser;
         alert(`Skynet: Ты все равно неудачник - я выбрал четное! \nнаш счет: \nКожаный - ${result.user} \nSkyNet - ${result.SkyNet}`);
       }
-      if (result.SkyNet === 0) {
-        alert(`Skynet: Я проиграл битву, но не войну!!! \nнаш счет: \nКожаный - ${result.user} \nSkyNet - ${result.SkyNet}`);
-        return;
-      }
-      if (result.user === 0) {
-        alert(`Skynet: Ты гребанный неудачник!=)) \nнаш счет: \nКожаный - ${result.user} \nSkyNet - ${result.SkyNet}`);
-        return;
+      if (result.SkyNet === 0 || result.user === 0) {
+        alert(`Skynet: Игра закончена! \nнаш счет: \nКожаный - ${result.user} \nSkyNet - ${result.SkyNet}`);
+        return choiceUser = false;
       }
     };
+
     winner();
+
     if (choiceUser) {
       return start();
     }
